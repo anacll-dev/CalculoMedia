bdfaa43542c746b136546186ef4332582f760fd9
Usuario <Usuario@CPD02-M11>
2023-05-31 11:40:13 -0300

    Adicionar arquivos de projeto.

---
+++ CalculoMedia/Form1.Designer.cs
@@ -0,0 +1,232 @@
+﻿namespace CalculoMedia
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.lblMedia = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btnCalcular = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtNota3 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtNota2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtNota1 = new System.Windows.Forms.TextBox();
+            this.btnSituacao = new System.Windows.Forms.Button();
+            this.lblSituacao = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.btnLimpar = new System.Windows.Forms.Button();
+            this.btnFechar = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.lblMedia);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.btnCalcular);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.txtNota3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.txtNota2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.txtNota1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(310, 242);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Notas:";
+            // 
+            // lblMedia
+            // 
+            this.lblMedia.AutoSize = true;
+            this.lblMedia.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblMedia.ForeColor = System.Drawing.Color.Gray;
+            this.lblMedia.Location = new System.Drawing.Point(162, 199);
+            this.lblMedia.Name = "lblMedia";
+            this.lblMedia.Size = new System.Drawing.Size(0, 24);
+            this.lblMedia.TabIndex = 8;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(98, 207);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(39, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Média:";
+            // 
+            // btnCalcular
+            // 
+            this.btnCalcular.Location = new System.Drawing.Point(110, 150);
+            this.btnCalcular.Name = "btnCalcular";
+            this.btnCalcular.Size = new System.Drawing.Size(90, 40);
+            this.btnCalcular.TabIndex = 6;
+            this.btnCalcular.Text = "Calcular Média";
+            this.btnCalcular.UseVisualStyleBackColor = true;
+            this.btnCalcular.Click += new System.EventHandler(this.btnCalcular_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(40, 113);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(42, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Nota 3:";
+            // 
+            // txtNota3
+            // 
+            this.txtNota3.Location = new System.Drawing.Point(88, 109);
+            this.txtNota3.Name = "txtNota3";
+            this.txtNota3.Size = new System.Drawing.Size(182, 20);
+            this.txtNota3.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(40, 75);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(42, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Nota 2:";
+            // 
+            // txtNota2
+            // 
+            this.txtNota2.Location = new System.Drawing.Point(88, 71);
+            this.txtNota2.Name = "txtNota2";
+            this.txtNota2.Size = new System.Drawing.Size(182, 20);
+            this.txtNota2.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(40, 38);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(42, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Nota 1:";
+            // 
+            // txtNota1
+            // 
+            this.txtNota1.Location = new System.Drawing.Point(88, 34);
+            this.txtNota1.Name = "txtNota1";
+            this.txtNota1.Size = new System.Drawing.Size(182, 20);
+            this.txtNota1.TabIndex = 0;
+            // 
+            // btnSituacao
+            // 
+            this.btnSituacao.Location = new System.Drawing.Point(122, 276);
+            this.btnSituacao.Name = "btnSituacao";
+            this.btnSituacao.Size = new System.Drawing.Size(90, 40);
+            this.btnSituacao.TabIndex = 9;
+            this.btnSituacao.Text = "Verificar Situação";
+            this.btnSituacao.UseVisualStyleBackColor = true;
+            this.btnSituacao.Click += new System.EventHandler(this.btnSituacao_Click);
+            // 
+            // lblSituacao
+            // 
+            this.lblSituacao.AutoSize = true;
+            this.lblSituacao.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSituacao.ForeColor = System.Drawing.Color.Blue;
+            this.lblSituacao.Location = new System.Drawing.Point(142, 337);
+            this.lblSituacao.Name = "lblSituacao";
+            this.lblSituacao.Size = new System.Drawing.Size(0, 24);
+            this.lblSituacao.TabIndex = 10;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(72, 345);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(52, 13);
+            this.label6.TabIndex = 9;
+            this.label6.Text = "Situação:";
+            // 
+            // btnLimpar
+            // 
+            this.btnLimpar.Location = new System.Drawing.Point(66, 381);
+            this.btnLimpar.Name = "btnLimpar";
+            this.btnLimpar.Size = new System.Drawing.Size(90, 40);
+            this.btnLimpar.TabIndex = 11;
+            this.btnLimpar.Text = "Novo Calculo";
+            this.btnLimpar.UseVisualStyleBackColor = true;
+            this.btnLimpar.Click += new System.EventHandler(this.btnLimpar_Click);
+            // 
+            // btnFechar
+            // 
+            this.btnFechar.Location = new System.Drawing.Point(178, 381);
+            this.btnFechar.Name = "btnFechar";
+            this.btnFechar.Size = new System.Drawing.Size(90, 40);
+            this.btnFechar.TabIndex = 12;
+            this.btnFechar.Text = "Fechar";
+            this.btnFechar.UseVisualStyleBackColor = true;
+            this.btnFechar.Click += new System.EventHandler(this.btnFechar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(334, 450);
+            this.Controls.Add(this.btnFechar);
+            this.Controls.Add(this.btnLimpar);
+            this.Controls.Add(this.lblSituacao);
+            this.Controls.Add(this.btnSituacao);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Calculo de Média";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtNota3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtNota2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtNota1;
+        private System.Windows.Forms.Label lblMedia;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btnCalcular;
+        private System.Windows.Forms.Button btnSituacao;
+        private System.Windows.Forms.Label lblSituacao;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button btnLimpar;
+        private System.Windows.Forms.Button btnFechar;
+    }
+}
+
---
+++ CalculoMedia/Form1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CalculoMedia
+{
+    public partial class Form1 : Form
+    {
+        double media = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalcular_Click(object sender, EventArgs e)
+        {
+            double nota1 = double.Parse(txtNota1.Text);
+            double nota2 = double.Parse(txtNota2.Text);
+            double nota3 = double.Parse(txtNota3.Text);
+
+            media = (nota1 + nota2 + nota3)/3;
+
+            lblMedia.Text = media.ToString("F3");
+            lblMedia.Visible = true;
+
+        }
+
+        private void btnSituacao_Click(object sender, EventArgs e)
+        {
+            if (media >= 6)
+            {
+                lblSituacao.Text = "APROVADO";
+                lblSituacao.ForeColor = Color.Green;
+            } else
+            {
+                lblSituacao.Text = "REPROVADO";
+                lblSituacao.ForeColor = Color.Red;
+            }
+            lblSituacao.Visible = true;
+
+        }
+
+        private void btnLimpar_Click(object sender, EventArgs e)
+        {
+            txtNota1.Text = string.Empty;
+            txtNota2.Text = string.Empty;
+            txtNota3.Text = string.Empty;
+            lblSituacao.Visible = false;
+            lblMedia.Visible = false;
+        }
+
+        private void btnFechar_Click(object sender, EventArgs e)
+        {
+            Environment.Exit(0);
+        }
+    }
+}
